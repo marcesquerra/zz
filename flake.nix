ce5c292fb313257efe2e7bc978b70b5052521338
Marc Esquerra <esquerra@gmail.com>
2025-07-23 22:02:22 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -2,15 +2,76 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     idr2nix.url = "git+https://git.sr.ht/~thatonelutenist/idr2nix?ref=trunk";
+    idr2nixSource = {
+      url = "git+https://git.sr.ht/~thatonelutenist/idr2nix?ref=trunk";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    rust_overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, idr2nix }: {
+  outputs = { self, nixpkgs, idr2nix, flake-utils, rust_overlay, idr2nixSource }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {inherit system;
+        overlays = [rust_overlay.overlays.default];
+      };
+      buildRustPlatform = dateOrVersion : channel : rustic :
+        let
+          rustChannel =
+            if channel == "stable" then rustic.rust-bin.stable.${dateOrVersion}
+            else rustic.rustChannelOf { inherit channel; date = dateOrVersion; };
+          rust = rustChannel.default;
+          nixRustPlatform = rustic.makeRustPlatform { cargo = rust; rustc = rust; };
+          getFromCargo = {src, cargoHash, nativeBuildInputs ? [], cargoBuildFlags ? []} :
+            let
+              lib = rustic.lib;
+              asName = candidates :
+                let
+                  ts = e: if (builtins.isAttrs e) && (builtins.hasAttr "name" e) && e.name != null then e.name else toString e;
+                  stringCandidates = builtins.map ts candidates;
+                  wholeString = lib.concatStrings stringCandidates;
+                in
+                  builtins.hashString "sha256" wholeString;
+            in
+              nixRustPlatform.buildRustPackage rec {
+                inherit src cargoHash nativeBuildInputs cargoBuildFlags;
+                pname = "cargo-${asName [src]}";
+                version = "N/A";
+                doCheck = false;
+              };
+        in {inherit rustChannel getFromCargo nixRustPlatform;};
+      rust1_76_0 = buildRustPlatform "1.76.0" "stable" pkgs;
+      getFromCargo = rust1_76_0.getFromCargo;
+      idr2nixBinary = getFromCargo {
+        src = idr2nixSource;
+        cargoHash = "sha256-93FtAbscmTNmAH8lED++SYU5gtoWTLIXt8OgSgkQF+Y=";
+        nativeBuildInputs = [
+          pkgs.openssl
+          pkgs.openssl.dev
+          pkgs.pkg-config
+        ];
+      };
+    in
+      rec {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        packages.${system} = {
+          hello = pkgs.hello;
+          default = self.packages.${system}.hello;
+        };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+        devShells = {
+          default = pkgs.mkShell {
+            packages = [
+              # idr2nix.packages.${system}.default
+              pkgs.hello
+              # idr2nixBinary
+            ];
+          };
+        };
 
-  };
+      }
+  );
 }
