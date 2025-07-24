3945b2cfdf4e233c6f8b463aa076c3d810c4c224
Marc Esquerra <esquerra@gmail.com>
2025-07-24 14:20:10 +0100

    add the idris dependency

--- flake.nix
+++ flake.nix
@@ -3,57 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    idr2nix.url = "git+https://git.sr.ht/~thatonelutenist/idr2nix?ref=trunk";
-    idr2nixSource = {
-      url = "git+https://git.sr.ht/~thatonelutenist/idr2nix?ref=trunk";
-      flake = false;
-    };
     flake-utils.url = "github:numtide/flake-utils";
-    rust_overlay.url = "github:oxalica/rust-overlay";
+    Idris2 = {
+      url = "github:idris-lang/Idris2";
+      flake = true;
+    };
   };
 
-  outputs = { self, nixpkgs, idr2nix, flake-utils, rust_overlay, idr2nixSource }:
+  outputs = { self, nixpkgs, flake-utils, Idris2 }:
   flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs {inherit system;
-        overlays = [rust_overlay.overlays.default];
-      };
-      buildRustPlatform = dateOrVersion : channel : rustic :
-        let
-          rustChannel =
-            if channel == "stable" then rustic.rust-bin.stable.${dateOrVersion}
-            else rustic.rustChannelOf { inherit channel; date = dateOrVersion; };
-          rust = rustChannel.default;
-          nixRustPlatform = rustic.makeRustPlatform { cargo = rust; rustc = rust; };
-          getFromCargo = {src, cargoHash, nativeBuildInputs ? [], cargoBuildFlags ? []} :
-            let
-              lib = rustic.lib;
-              asName = candidates :
-                let
-                  ts = e: if (builtins.isAttrs e) && (builtins.hasAttr "name" e) && e.name != null then e.name else toString e;
-                  stringCandidates = builtins.map ts candidates;
-                  wholeString = lib.concatStrings stringCandidates;
-                in
-                  builtins.hashString "sha256" wholeString;
-            in
-              nixRustPlatform.buildRustPackage rec {
-                inherit src cargoHash nativeBuildInputs cargoBuildFlags;
-                pname = "cargo-${asName [src]}";
-                version = "N/A";
-                doCheck = false;
-              };
-        in {inherit rustChannel getFromCargo nixRustPlatform;};
-      rust1_76_0 = buildRustPlatform "1.76.0" "stable" pkgs;
-      getFromCargo = rust1_76_0.getFromCargo;
-      idr2nixBinary = getFromCargo {
-        src = idr2nixSource;
-        cargoHash = "sha256-93FtAbscmTNmAH8lED++SYU5gtoWTLIXt8OgSgkQF+Y=";
-        nativeBuildInputs = [
-          pkgs.openssl
-          pkgs.openssl.dev
-          pkgs.pkg-config
-        ];
-      };
+      pkgs = import nixpkgs {inherit system;};
     in
       rec {
 
@@ -65,9 +25,7 @@
         devShells = {
           default = pkgs.mkShell {
             packages = [
-              # idr2nix.packages.${system}.default
-              pkgs.hello
-              # idr2nixBinary
+              pkgs.hello Idris2.packages.${system}.idris2
             ];
           };
         };
