8fdac55591a6e8e1e06cb4f77e8eeb29b0e60083
Marc Esquerra <esquerra@gmail.com>
2025-07-26 09:55:39 +0100

    z

--- shell.nix
+++ shell.nix
@@ -1,8 +1,17 @@
 { pkgs, idris2-pkgs } :
 let
+  git_z = pkgs.writeShellScriptBin "git-z" ''
+
+    cd $WORKSPACE
+
+    git add .
+
+    git commit -m "z" && git push
+  '';
 in
   pkgs.mkShell {
     packages = [
-      pkgs.hello idris2-pkgs.idris2
+      idris2-pkgs.idris2
+      git_z
     ];
   }
