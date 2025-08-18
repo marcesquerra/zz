cd1bf80a95e7a7a582e9cc4551d7e5781384f27f
Marc Esquerra <esquerra@gmail.com>
2025-08-18 17:31:45 +0100

    Start preparing for going public

--- shell.nix
+++ shell.nix
@@ -1,20 +1,11 @@
 { pkgs, idris2-pkgs } :
 let
   idris2-pkg = idris2-pkgs.idris2;
-  git_z = pkgs.writeShellScriptBin "git-z" ''
-
-    cd $WORKSPACE
-
-    git add .
-
-    git commit -m "z" && git push
-  '';
   iw = import nix/iw.nix pkgs idris2-pkg;
 in
   pkgs.mkShell {
     packages = [
       idris2-pkg
-      git_z
       iw
     ];
   }
